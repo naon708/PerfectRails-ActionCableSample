7332070e397e2a2697c49ca6c1f4d63787b7ce87
naon708 <naoya_nis@icloud.com>
2023-04-06 22:29:05 +0900

    BE: クライアントからメッセージが届いた時、通信しているすべてのクライアントに届いたメッセージを送信する処理を追加した

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,7 +1,7 @@
 class RoomChannel < ApplicationCable::Channel
   # 購読後に呼ばれる
   def subscribed
-    # stream_from "some_channel"
+    stream_from "room_channel"
   end
 
   # 購読解除後に呼ばれる
@@ -10,6 +10,9 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   # クライアントサイドから呼び出された時に実行される
-  def speak
+  def speak(data)
+    ActionCable.server.broadcast(
+      "room_channel", { message: data["message"] }
+    )
   end
 end
