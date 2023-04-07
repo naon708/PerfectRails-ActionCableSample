cae8a5e162381f64f47d0659ac962dc095023d28
naon708 <naoya_nis@icloud.com>
2023-04-07 23:09:25 +0900

    コネクションインスタンスを生成することで、User単位の接続情報を管理できるようにした

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    # current_user および current_user= が作成される
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      if verified_user = User.find_by(id: cookies.signed[:user_id])
+        verified_user
+      else
+        # WebSocket接続を取りやめるためのメソッド
+        reject_unauthorized_connection
+      end
+    end
   end
 end
