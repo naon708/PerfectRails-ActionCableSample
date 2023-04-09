1d058a970c334daeff70ff888b6989b698376523
naon708 <naoya_nis@icloud.com>
2023-04-09 20:57:36 +0900

    チャネルのテストを追加

--- test/channels/room_channel_test.rb
+++ test/channels/room_channel_test.rb
@@ -1,8 +1,26 @@
 require "test_helper"
 
 class RoomChannelTest < ActionCable::Channel::TestCase
-  # test "subscribes" do
-  #   subscribe
-  #   assert subscription.confirmed?
-  # end
+  # チャネルのテスト
+  test "subscribes" do
+    subscribe
+    # subscriptionメソッドでRoomChannelオブジェクトを取得できる
+    assert subscription.confirmed?
+    # room_channelが作成されているかどうか
+    assert_has_stream "room_channel"
+  end
+
+  # ブロードキャストのテスト
+  test "broadcasts" do
+    subscribe
+    text = "hello"
+    broadcast_text = ApplicationController.render(
+      partial: 'messages/message',
+      locals: { message: Message.new(content: text) }
+    )
+    assert_broadcast_on('room_channel', message: broadcast_text) do
+      # クライアント側からspeakメソッドを呼び出して、渡した文字列を含むHTMLがブロードキャストされたかを確認する
+      perform :speak, message: text
+    end
+  end
 end
